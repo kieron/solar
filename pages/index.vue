6ba1f5fa99d6ce16597f6860740d9e9adeb5ddf4
kieron <103420515+kieronasura@users.noreply.github.com>
2023-04-18 20:19:57 +0100

    tweaks (#1)

--- pages/index.vue
+++ pages/index.vue
@@ -7,28 +7,6 @@ definePageMeta({
   layout: 'page',
 })
 
-// vars
-const titlesText = computed<string[]>(() => t('pages.index.title').split('[]'))
-const leadingsText = computed(() => [
-  {
-    text: titlesText.value[0],
-    startColor: '#007CF0',
-    endColor: '#00DFD8',
-    delay: 0,
-  },
-  {
-    text: titlesText.value[1],
-    startColor: '#7928CA',
-    endColor: '#FF0080',
-    delay: 2,
-  },
-  {
-    text: titlesText.value[2],
-    startColor: '#FF4D4D',
-    endColor: '#F9CB28',
-    delay: 4,
-  },
-])
 const tooltip = ref(false)
 
 // const
@@ -65,16 +43,9 @@ const copyBash = () => {
         <div class="flex-1 md:w-5/8 flex flex-col z-10">
           <h1 class="text-center md:text-left mt-4">
             <span
-              v-for="(item, i) in leadingsText"
-              :key="i"
-              :style="`--content: '${item.text}'; --start-color: ${
-                item.startColor
-              }; --end-color: ${item.endColor}; --animation-name: anim-fg-${
-                i + 1
-              }`"
               class="animated-text-bg drop-shadow-xl text-5xl xl:text-8xl 2xl:text-9xl block font-black uppercase"
             >
-              <span class="animated-text-fg">{{ item.text }}</span>
+              <span class="">Find your dream job in Solar</span>
             </span>
           </h1>
           <div
